87243dbbe9b2b48c5de42e3749bc92557a5fbdac
Sheldon Jatou <sheldonjatou@gmail.com>
2016-12-25 17:13:10 -0500

    Grade adder

--- GUI_Main.py
+++ GUI_Main.py
@@ -7,14 +7,17 @@ from tkinter import *
 
 main = Tk()
 
-g = Label(main, text='GRADE')
-w = Label(main, text='WEIGHT')
+top = Frame(main)
+top.pack()
+
+g = Label(top, text='GRADE')
+w = Label(top, text='WEIGHT')
 
 g.grid(row=0, sticky=W)
-w.grid(row=0, column=2, sticky=W)
+w.grid(row=0, column=2, sticky=E)
 
-grades = [Entry(main), Entry(main), Entry(main)]
-weights = [Entry(main), Entry(main), Entry(main)]
+grades = [Entry(top), Entry(top), Entry(top)]
+weights = [Entry(top), Entry(top), Entry(top)]
 
 for i in range(len(grades)):
     grades[i].grid(row=i + 1)
@@ -22,9 +25,18 @@ for i in range(len(grades)):
 for i in range(len(weights)):
     weights[i].grid(row=i + 1, column=2)
 
-f = 0
-final = Label(main, text='FINAL GRADE: {}'.format(f))
-final.grid(column=1)
+
+def add_grade():
+    grades.append(Entry(top))
+    weights.append(Entry(top))
+
+    grades[-1].grid(row=len(grades))
+    weights[-1].grid(row=len(weights), column=2)
+
+    print(len(grades))
+
+add = Button(top, text='ADD GRADE', command=add_grade)
+add.grid(row=0, columnspan=4)
 
 
 def calculate_average():
@@ -32,19 +44,28 @@ def calculate_average():
     global f
 
     for i in range(len(grades)):
-        if grades[i].get() != '' and weights[i].get() != '':
-            total = (float(grades[i].get()) / 100) * \
-                (float(weights[i].get()) / 100)
-            totals.append(total)
-        elif (grades[i].get() == '' or weights[i].get() == '') and (grades[i].get() != '' and weights[i].get() != ''):
+        try:
+            if grades[i].get() != '' and weights[i].get() != '':
+                total = (float(grades[i].get()) / 100) * \
+                    (float(weights[i].get()) / 100)
+                totals.append(total)
+        except ValueError:
             pass
 
     f = round(sum(totals) * 100, 1)
+
     final.config(text='FINAL GRADE: {}'.format(f))
 
+bottom = Frame(main)
+bottom.pack(side=BOTTOM)
 
-calc = Button(main, text='CALCULATE TOTAL', command=calculate_average)
-calc.grid(columnspan=4)
+f = 0
+final = Label(bottom, text='FINAL GRADE: {}'.format(f))
+final.grid(column=1)
+
+
+calc = Button(bottom, text='CALCULATE TOTAL', command=calculate_average)
+calc.grid(columnspan=4, sticky=S)
 
 
 main.mainloop()
